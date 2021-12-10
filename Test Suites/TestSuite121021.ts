9891eb110320f10b61c8eb546746807ab82a308c
Nguyen Van Minh <minh.nguyen@katalon.com>
2021-12-10 15:05:06 +0700

    first commit

---
+++ Test Suites/TestSuite121021.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite121021</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6a167ed7-3d57-4dda-a873-3cf3d1984c0f</testSuiteGuid>
+   <testCaseLink>
+      <guid>8faf4820-10e2-42d5-a845-99a0b218f661</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test.Case.121021/Folder1.Folder2.Folder3/Form1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>99febb4b-af00-4170-a552-2fab35627d6e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test.Case.121021/Folder1.Folder2.Folder3/Form121021</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>529aa039-36da-4578-ac60-00ebf43802e9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test.Case.121021/Folder4/Form121021a</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>54cb1ed3-d24e-4c57-a446-d9a69189a43b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test.Case.121021/Folder4/Form121021b</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
