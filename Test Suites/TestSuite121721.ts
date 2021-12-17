e8475745f4217c5e4abd5a39e0f9a8398dd500bc
Nguyen Van Minh <minh.nguyen@katalon.com>
2021-12-17 15:47:21 +0700

    first commit

--- Test Suites/TestSuite121721.ts
+++ Test Suites/TestSuite121721.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>TestSuite121021</name>
+   <name>TestSuite121721</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -15,24 +15,24 @@
       <guid>8faf4820-10e2-42d5-a845-99a0b218f661</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Test.Case.121021/Folder1.Folder2.Folder3/Form1</testCaseId>
+      <testCaseId>Test Cases/Test.Case.121721a/Folder1.Folder2a/Form121721b</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>99febb4b-af00-4170-a552-2fab35627d6e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Test.Case.121021/Folder1.Folder2.Folder3/Form121021</testCaseId>
+      <testCaseId>Test Cases/Test.Case.121721a/Folder1.Folder2a/Form121721a</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>529aa039-36da-4578-ac60-00ebf43802e9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Test.Case.121021/Folder4/Form121021a</testCaseId>
+      <testCaseId>Test Cases/Test.Case.121721a/Folder3.Folder4a/Form121721aa</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>54cb1ed3-d24e-4c57-a446-d9a69189a43b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Test.Case.121021/Folder4/Form121021b</testCaseId>
+      <testCaseId>Test Cases/Test.Case.121721a/Folder3.Folder4a/Form121721ba</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
--- Test Suites/TestSuiteCollection121721.ts
+++ Test Suites/TestSuiteCollection121721.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteCollectionEntity>
    <description></description>
-   <name>TestSuiteCollection121021</name>
+   <name>TestSuiteCollection121721</name>
    <tag></tag>
    <executionMode>SEQUENTIAL</executionMode>
    <maxConcurrentInstances>8</maxConcurrentInstances>
@@ -13,7 +13,16 @@
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
-         <testSuiteEntity>Test Suites/TestSuite121021</testSuiteEntity>
+         <testSuiteEntity>Test Suites/TestSuite121721</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <runConfigurationId>Chrome</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/TestSuite121721</testSuiteEntity>
       </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
 </TestSuiteCollectionEntity>
